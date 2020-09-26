5a8a6543eac378ec9eb48ccd09482dbad959313b
Ashutosh Srivastava <ashutosh65000@outlook.com>
2020-09-26 22:29:00 +0530

    Static and Network Images

--- App.js
+++ App.js
@@ -8,6 +8,24 @@ export default function App() {
       <Text numberOfLines={2} selectable={true} selectionColor={"#99cc77"}>
         Hello there. This is a very long text. I want to make this text very very very long so that it takes atleast two lines.
       </Text>
+      {/* static images*/}
+      <Image
+        blurRadius={1}
+        source={
+          require("./assets/favicon.png")
+        }
+      />
+      {/* network images */}
+      <Image
+        fadeDuration={3000}
+        source={
+          {
+            width: 200,
+            height: 300,
+            uri: "https://2.bp.blogspot.com/-cIjErjDVzYg/XMXsHNqcjCI/AAAAAAAAE8k/q1J6yx82Q8EEbGk2XbKIbzwHnsg-z4XBgCEwYBhgL/s1600/Karishma%2BSharma%2B-%2Bhot%2Bindian%2Bactress%2B-%2Bbra%2B-%2Bragini%2Bmms%2Breturns%2B%252820%2529.jpg"
+          }
+        }
+      />
       <StatusBar style="auto" />
     </View>
   );
