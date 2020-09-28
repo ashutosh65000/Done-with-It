c4d436096d599771061978ace4408c2bc99995e9
Ashutosh Srivastava <ashutosh65000@outlook.com>
2020-09-28 16:53:04 +0530

    inline stylesheet

--- App.js
+++ App.js
@@ -1,33 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableHighlight, Button, Alert, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, Image, TouchableHighlight, Button, Alert } from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      {/* <Text numberOfLines={2} selectable={true} selectionColor={"#99cc77"}>
-        Hello there. This is a very long text. I want to make this text very very very long so that it takes atleast two lines.
-      </Text> */}
+    <SafeAreaView style={{ backgroundColor: "orange" }}>
       <Button
+        color="magenta"
         title="Click Me"
         onPress={() =>
-          Alert.prompt()
-        }
+          Alert.alert(
+            "Favourite Movie",
+            "Avengers Assemble."
+          )}
       />
-      {/* <TouchableHighlight onPress={() => console.log("Image Pressed")}> */}
-      {/* network images */}
-      {/* <Image
-          fadeDuration={3000}
-          source={
-            {
-              width: 200,
-              height: 300,
-              uri: "https://2.bp.blogspot.com/-cIjErjDVzYg/XMXsHNqcjCI/AAAAAAAAE8k/q1J6yx82Q8EEbGk2XbKIbzwHnsg-z4XBgCEwYBhgL/s1600/Karishma%2BSharma%2B-%2Bhot%2Bindian%2Bactress%2B-%2Bbra%2B-%2Bragini%2Bmms%2Breturns%2B%252820%2529.jpg"
-            }
-          }
-        /> */}
-      {/* </TouchableHighlight> */}
-      {/* <StatusBar style="auto" /> */}
+      <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
