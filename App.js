a789e0104a9cfe2d3ea29be3a13c3f864ccf40f8
Ashutosh Srivastava <ashutosh65000@outlook.com>
2020-09-29 00:21:26 +0530

    WelcomeScreen of app

--- App.js
+++ App.js
@@ -1,38 +1,8 @@
 import React from 'react';
 import { View } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: "#fff",
-        flex: 1,
-        flexDirection: "row", //horizontal
-        justifyContent: "center", //main
-        alignItems: "center", //secondary
-      }}>
-      <View
-        style={{
-          backgroundColor: "gold",
-          width: 100,
-          height: 100
-        }} />
-      <View
-        style={{
-          backgroundColor: "magenta",
-          width: 100,
-          height: 100,
-          bottom: 20,
-          left: 20,
-          position: "absolute",
-        }} />
-      <View
-        style={{
-          backgroundColor: "tomato",
-          width: 100,
-          height: 100,
-        }} />
-    </View>
-  )
+  return (<WelcomeScreen />)
 }
