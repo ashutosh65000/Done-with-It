b107f0beb5b0a47bf9014c38723deb7157ac52d3
Ashutosh Srivastava <ashutosh65000@outlook.com>
2020-09-30 22:59:26 +0530

    padding and margin styling

--- App.js
+++ App.js
@@ -20,14 +20,26 @@ export default function App() {
           backgroundColor: "red",
           width: 100,
           height: 100,
-          // for IOS three properties
-          shadowColor: "grey",
-          shadowOffset: { height: 20, width: -20 }, // negative for the reverse of axis like the two plane charts
-          shadowOpacity: 1,
-          // for Android single property; as the android doesn't provides much control over the shadows color and opacity.
-          elevation: 30
         }}
-      />
+      >
+        <View
+          style={{
+            backgroundColor: "green",
+            width: 50,
+            height: 50,
+            paddingHorizontal: 20,
+            paddingTop: 20
+          }}
+        ></View>
+      </View>
+      <View
+        style={{
+          backgroundColor: "magenta",
+          width: 100,
+          height: 100,
+          margin: 20
+        }}
+      ></View>
     </View>
   )
 }
