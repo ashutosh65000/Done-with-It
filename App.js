56d75d56a438dbcdf471ba995951778b09bd5a73
Ashutosh Srivastava <ashutosh65000@outlook.com>
2020-09-30 22:43:56 +0530

    border and border radiusstyling

--- App.js
+++ App.js
@@ -6,5 +6,27 @@ import ViewImageScreen from './app/screens/ViewImageScreen';
 
 export default function App() {
   // return (<WelcomeScreen />)
-  return (<ViewImageScreen />)
+  // return (<ViewImageScreen />)
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "red",
+          width: 100,
+          height: 100,
+          borderBottomColor: "green",
+          borderRadius: 50,
+          borderWidth: 10,
+          borderTopStartRadius: 20,
+          borderTopRightRadius: 20,
+        }}
+      />
+    </View>
+  )
 }
